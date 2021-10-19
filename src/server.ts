d5e47d7b5b9788ff848a4ef8ee8ef9dc687b29e6
Biz1999 <alessanbiz@gmail.com>
2021-10-19 16:01:16 -0300

    react app added to backend

--- src/server.ts
+++ src/server.ts
@@ -14,6 +14,10 @@ const app = express();
 const bootstrap = async () => {
   await postgreDB();
 
+  const https = require("https");
+  const fs = require("fs");
+  const path = require("path");
+
   app.use(cors());
   app.use(express.json());
 
@@ -25,6 +29,15 @@ const bootstrap = async () => {
       "../../../SPI Integracao de Sistemas Ltda/Superar-Para-Inovar-CDN - API-Armazenamento"
     )
   );
+  app.use("/", express.static("public"));
+
+  app.get("*", function (req, res) {
+    res
+      .status(404)
+      .send(
+        "<div style='margin: auto;width: 100%;height:100%;text-align: center;background-color:#651D32'><h1 style='color:white'>404 NOT FOUND</h1><img src='https://static.wixstatic.com/media/a04754_ae492f92043d473d89fba314c59f9c7c~mv2.png/v1/fill/w_156,h_102,al_c,q_85,usm_0.66_1.00_0.01/Superar_Inovar_SPI_Logo_2021-04.webpg' width=100/></div>"
+      );
+  });
 
   app.use(
     (err: Error, request: Request, response: Response, next: NextFunction) => {
@@ -39,9 +52,21 @@ const bootstrap = async () => {
       });
     }
   );
-  app.listen(8000, ip.address(), () =>
-    console.log(`Port ${ip.address()}-> Server listenin...`)
-  );
+
+  // const sslServer = https.createServer(
+  //   {
+  //     key: fs.readFileSync(path.join(__dirname, "../cert", "key.pem")),
+  //     cert: fs.readFileSync(path.join(__dirname, "../cert", "cert.pem")),
+  //   },
+  //   app
+  // );
+
+  // sslServer.listen(8000, () => {
+  //   console.log(`Ip ${ip.address()}-> Server listenin...`);
+  // });
+  app.listen(8000, () => {
+    console.log(`Ip ${ip.address()}-> Server listenin...`);
+  });
 };
 
 bootstrap();
